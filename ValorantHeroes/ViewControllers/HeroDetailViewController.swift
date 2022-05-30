9917ede88fa0aa93a0d62beb8220665f05cf0a25
Селбаев Алмас <almasselbayev@gmail.com>
2022-05-31 00:42:40 +0600

    added SegmentedControl for ability and it's description

--- ValorantHeroes/ViewControllers/HeroDetailViewController.swift
+++ ValorantHeroes/ViewControllers/HeroDetailViewController.swift
@@ -15,12 +15,48 @@ class HeroDetailViewController: UIViewController {
     @IBOutlet var nameLabel: UILabel!
     @IBOutlet var descriptionLabel: UILabel!
     
+    @IBOutlet var abilitySegmentControl: UISegmentedControl!
+    @IBOutlet var abilityTextLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         roleLabel.text = heroDetail.role.displayName
         nameLabel.text = String.uppercased(heroDetail.displayName)()
         descriptionLabel.text = heroDetail.description
-        
+        setUpAbilitySC()
+    }
+    
+    @IBAction func abilitySelectAction() {
+        if abilitySegmentControl.selectedSegmentIndex != 0 {
+            abilityTextLabel.text = heroDetail.abilities[abilitySegmentControl.selectedSegmentIndex-1].description
+        } else {
+            abilityTextLabel.text = heroDetail.role.description
+        }
         
     }
+    
+    private func setUpAbilitySC() {
+        var count = 1
+        
+        abilitySegmentControl.removeAllSegments()
+        
+        NetworkManager.shared.fetchImage(from: heroDetail.role.displayIcon) { icon in
+            self.abilitySegmentControl.insertSegment(with: UIImage(data: icon), at: 0, animated: true)
+            self.abilitySegmentControl.selectedSegmentIndex = 0
+            self.abilitySelectAction()
+        }
+        
+        for ability in heroDetail.abilities {
+            NetworkManager.shared.fetchImage(from: ability.displayIcon) { icon in
+                self.abilitySegmentControl.insertSegment(with: UIImage(data: icon), at: count, animated: true)
+                
+                //self.abilitySegmentControl.frame.size.width = 200
+                //self.abilitySegmentControl.frame.size.height = 200
+                
+                count += 1
+            }
+        }
+    }
+    
+    
 }
